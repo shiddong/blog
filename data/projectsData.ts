4ae2fa02a99989836ce4c79b44b403f6433c2312
Richard Shih <shiddong0724@gmail.com>
2023-11-06 12:10:56 +0800

    feat: 改造页面

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,11 +1,11 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
+    title: 'Collectin',
     description: `What if you could look up any information in the world? Webpages, images, videos
     and more. Google has many features to help you find exactly what you're looking
     for.`,
     imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    href: 'https://collectin.net',
   },
   {
     title: 'The Time Machine',
